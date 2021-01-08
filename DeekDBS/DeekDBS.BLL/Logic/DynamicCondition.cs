b587e1652655f0e33207ba8d63fbdfbbbfe300c4
Caybo Kotze <kotzecabo@gmail.com>
2021-01-08 16:02:20 +0200

    Add Reflective Condition class

---
+++ DeekDBS/DeekDBS.BLL/Logic/DynamicCondition.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace DeekDBS.BLL.Logic
+{
+    public static class DynamicReflectiveConditions
+    {
+        public static bool Evalaute(string compare, string to)
+        {
+            if (compare.Equals(to))
+                return true;
+
+            return false;
+        }
+
+        public static bool Evaluate(string argument)
+        {
+            string source =
+                @"
+namespace Compare
+{   
+    public class Comparison
+    {
+        public bool Evaluator()
+        {   
+            if("+argument+@")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}";
+
+            Dictionary<string, string> providerOptions = 
+                new Dictionary<string, string>
+            {
+                {"CompilerVersion", "v3.5" }
+            };
+
+            CSharpCodeProvider provider = 
+                new CSharpCodeProvider(providerOptions);
+
+            var compilerParams = new CompilerParameters
+            {
+                GenerateInMemory = true,
+                GenerateExecutable = false
+            };
+
+            CompilerResults results = provider.CompileAssemblyFromSource(compilerParams, source);
+
+            if(results.Errors.Count != 0)
+            {
+                throw new Exception("Err, something went horribly wrong.");
+            }
+
+            object o = results.CompiledAssembly.CreateInstance("Compare.Comparison");
+            MethodInfo mi = o.GetType().GetMethod("Evaluator");
+            mi.Invoke(o, null);
+
+            return false;
+        }
+    }
+}
---
+++ DeekDBS/DeekDBS.BLL/Models/MappingConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DeekDBS.BLL.Models
+{
+    class MappingConfiguration
+    {
+        public int Id { get; set; }
+        public string SerialzedMappings { get; set; }
+    }
+
+    class SerializedMappingStore
+    {
+        DataTable dt = new DataTable();
+        void Configure()
+        {
+            dt.Rows.Add();
+        }
+        public int OrderId { get; set; }
+        public string PrimaryMappings { get; set; }
+        public string SecondaryMapping { get; set; }
+        public string Operative { get; set; }
+        public string Evaluation { get; set; }
+    }
+}
